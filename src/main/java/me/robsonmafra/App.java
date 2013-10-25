ff0c9f5bea7ffcc9b9b231c826d7c63df342803f
Robson Valdir Mafra <rubisso@gmail.com>
2013-10-25 19:06:22 +0100

    Created a sample application to search for twitter with the word "java".

--- src/main/java/me/robsonmafra/App.java
+++ src/main/java/me/robsonmafra/App.java
@@ -1,5 +1,7 @@
 package me.robsonmafra;
 
+import me.robsonmafra.twitter.TwitterFilterStream;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String[] words = {"java"};
+        TwitterFilterStream stream = new TwitterFilterStream(words);
+        stream.start();
     }
 }
